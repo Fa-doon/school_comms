1696466c9548e124426301d101c588de2e59571c
Fadoon T <fadoon.tar@gmail.com>
2024-05-21 14:36:41 +0100

    added admin chatrooms route

--- routes/chatRoute.js
+++ routes/chatRoute.js
@@ -5,12 +5,13 @@ const {
   getChatMessagesByRoomId,
   getAllChatrooms,
 } = require("../controllers/chatController");
-const { isUser } = require("../middlewares/authMiddleware");
+const { isUser, isAdmin } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
 router.post("/chatroom", isUser, createChatroom);
 router.post("/messages", isUser, sendMessage);
 router.get("/messages/:roomName", getChatMessagesByRoomId);
 router.get("/user-chatrooms", isUser, getAllChatrooms);
+router.get("/admin-chatrooms", isAdmin, getAllChatrooms);
 
 module.exports = router;
